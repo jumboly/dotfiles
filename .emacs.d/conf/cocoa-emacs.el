768fc3737099ceaa315e8804b347264109d21d24
Masanori Kitani <jumboly@gmail.com>
2012-03-11 00:12:02 +0900

    second

--- .emacs.d/conf/cocoa-emacs.el
+++ .emacs.d/conf/cocoa-emacs.el
@@ -1,4 +1,2 @@
-;; macのemacsでファイル名を正しく扱うための設定
-(require 'ucs-normalize)
-(setq file-name-coding-system 'utf-8-hfs)
-(setq local-cofing-system 'utf-8-hfs)
+;; path
+(add-to-list 'exec-path "usr/local/bin")
