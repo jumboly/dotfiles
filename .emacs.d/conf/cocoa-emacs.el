9dbf1a43bb4eeee0abf55b405f35796123c3723e
Masanori Kitani <jumboly@gmail.com>
2012-03-10 21:28:34 +0900

    First commit

---
+++ .emacs.d/conf/cocoa-emacs.el
@@ -0,0 +1,4 @@
+;; macのemacsでファイル名を正しく扱うための設定
+(require 'ucs-normalize)
+(setq file-name-coding-system 'utf-8-hfs)
+(setq local-cofing-system 'utf-8-hfs)
---
+++ .emacs.d/init.el
@@ -0,0 +1,20 @@
+;言語を日本語にする
+(set-language-environment 'Japanese)
+
+;; load-path を追加する関数を定義
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+      (let ((default-directory
+	      (expand-file-name (concat user-emacs-directory path))))
+	(add-to-list 'load-path default-directory)
+	(if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+	    (normal-top-level-add-subdirs-to-load-path))))))
+
+;; 引数のディレクトリとそのサブディレクトリをload-pathに追加
+(add-to-load-path "elisp" "conf" "public_repos")
+
+;; http://coderepos.org/share/browser/lang/elisp/init-loader/init-loader.el
+(require 'init-loader)
+(init-loader-load "~/.emacs.d/conf")
+
